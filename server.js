38b836bc854c6e99574a38cbce750385a66eb226
julieritz <julie.ritz@gmail.com>
2020-06-30 11:29:20 -0700

    small server change, adding model

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
+
 const app = express();
 
 app.use(morgan("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-mongoose.connect(URI, {
-    useNewUrlParser: true
-});
+const db = require("./models");
 
 app.use(require("./routes/apiRoutes.js"));
 app.use(require("./routes/htmlRoutes.js"));
